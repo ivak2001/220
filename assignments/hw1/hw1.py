828ed59c9041af9787c3e51b07771faa786170a5
ivak2001 <ikaralic24@gmail.com>
2022-01-21 12:15:27 -0800

    Complete hw 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,49 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Iva Karalic
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Calculate volume and area by asking the user for input.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total = eval(input("Enter total shots: "))
+    made = eval(input("Enter shots made: "))
+    percentage = (made / total) * 100
+    print("Shooting Percentage =", percentage, "%")
 
 
 def coffee():
-    pass
+    pounds = eval(input("Number of pounds ordered: "))
+    beans = 10.50
+    shipping = 0.86
+    order = 1.50
+    total = (pounds * beans) + (pounds * shipping) + order
+    print("Total Cost = $", total)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("Enter kilometers: "))
+    miles = kilometers / 1.61
+    print("Miles =", miles)
 
 
 if __name__ == '__main__':
