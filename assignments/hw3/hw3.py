0f6e4d4103f7900a7fd0c9bd994b8a4ed0283c10
ivak2001 <ikaralic24@gmail.com>
2022-02-04 13:55:58 -0500

    complete hw 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,56 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Iva Karalic
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Using loops and user input to calculate the answers.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    x = eval(input("How many grades will you enter? "))
+    acc = 0
+    for i in range(1, x + 1):
+        grade = eval(input("Enter grade: "))
+        acc = acc + grade
+        final = acc / x
+    print(final)
 
 
 def tip_jar():
-    pass
+    acc = 0
+    for i in range(5):
+        amount = eval(input("How much would you like to donate? "))
+        acc = acc + amount
+    print(acc)
 
 
 def newton():
-    pass
+    x = eval(input("What number do you want to square root? "))
+    improve = eval(input("How many times should we improve the approximation? "))
+    approx = x / 2
+    for i in range(improve):
+        approx = ((approx + (x / approx)) / 2)
+    print("The square root is approximately ", approx)
 
 
 def sequence():
-    pass
+    x = eval(input("How many terms would you like? "))
+    for i in range(x + 1):
+        print(1 + (i // 2 * 2), end=" ")
 
 
 def pi():
-    pass
+    n = eval(input("How many terms in the series? "))
+    acc = 1
+    for x in range(n):
+        numerator = 2 + ((x // 2) * 2)
+        denominator = 1 + (((x + 1) // 2) * 2)
+        f = numerator / denominator
+        acc = acc * f
+    print(acc * 2)
 
 
 if __name__ == '__main__':
